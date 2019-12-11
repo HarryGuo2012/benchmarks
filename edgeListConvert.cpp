0b1dece5e333b315b1fbf7e5ddeba2b2b41dc9e4
HarrySurfaceBookWSL <Harry.Guo@outlook.com>
2019-12-11 15:45:49 +0800

    add cpp convert

---
+++ edgeListConvert.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+char buff[256];
+std::stringstream ss;
+
+std::vector<int> vertexList;
+std::vector<std::pair<int, int> > edgeList;
+
+// input file path and output file path
+int main(int argc, char *argv[]) {
+    std::ifstream ifs(argv[1]);
+    std::ofstream ofs(argv[2]);
+
+    while (ifs.getline(buff, 256)) {
+        if (buff[0] == '%' || buff[0] < '0' || '9' < buff[0]) continue;
+
+        int u, v;
+        ss.clear();
+        ss << buff;
+        ss >> u >> v;
+
+        if (u == v) continue; // self loop
+
+        vertexList.push_back(u);
+        vertexList.push_back(v);
+
+        if (u > v) std::swap(u, v);
+
+        edgeList.push_back(std::make_pair(u, v));
+    }
+
+    // Hash vertex
+
+    std::sort(vertexList.begin(), vertexList.end());
+    auto it = std::unique(vertexList.begin(), vertexList.end());
+    vertexList.resize(std::distance(vertexList.begin(), it));
+
+    for (auto &e : edgeList) {
+        int &u = e.first;
+        int &v = e.second;
+
+        u = std::lower_bound(vertexList.begin(), vertexList.end(), u) - vertexList.begin() + 1;
+        v = std::lower_bound(vertexList.begin(), vertexList.end(), v) - vertexList.begin() + 1;
+    }
+
+    std::sort(edgeList.begin(), edgeList.end());
+    auto jt = std::unique(edgeList.begin(), edgeList.end());
+    edgeList.resize(std::distance(edgeList.begin(), jt));
+
+    int n = vertexList.size();
+    int m = edgeList.size();
+
+    ofs << n << " " << m << std::endl;
+    for (auto e : edgeList) ofs << e.first << " " << e.second << std::endl;
+    return 0;
+}
---
+++ mtxListConvert.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+char buff[256];
+
+int n, m;
+
+std::stringstream ss;
+
+int main(int argc, char *argv[]) {
+    std::ifstream ifs(argv[1]);
+    std::ofstream ofs(argv[2]);
+
+    int cnt = 0;
+
+    while (ifs.getline(buff, 256)) {
+        if (buff[0] == '%') continue;
+
+        cnt = cnt + 1;
+
+        ss.clear();
+        ss << buff;
+
+        if (cnt == 1) {
+            int t;
+            ss >> t >> n >> m;
+            ofs << n << " " << m << std::endl;
+            continue;
+        }
+
+        int u, v;
+        ss >> u >> v;
+        ofs << u << " " << v << std::endl;
+        
+    }
+    return 0;
+}
