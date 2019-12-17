8908d066b470e4ca040ec33c80cf108b1a8c2a6a
HarrySurfaceBookWSL <Harry.Guo@outlook.com>
2019-12-17 15:55:52 +0800

    update moving.py

---
+++ moving.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from shutil import copyfile
+
+f = open('big_list.txt', 'r')
+
+lines = f.readlines()
+haha = set()
+
+for line in lines:
+    haha.add(line + '.txt')
+
+if not os.path.isdir('big_temp'):
+    os.mkdir('big_temp')
+
+for root, dire, filename in os.walk('.'):
+    if filename in haha:
+        copyfile(os.path.join(root, filename), os.path.join('big_temp', filename))
+
