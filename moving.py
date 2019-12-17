90375e06391ef84d32dbeba6c41e2f71d1eaeedd
HarrySurfaceBookWSL <Harry.Guo@outlook.com>
2019-12-17 16:06:44 +0800

    fuck

--- moving.py
+++ moving.py
@@ -8,12 +8,12 @@ lines = f.readlines()
 haha = set()
 
 for line in lines:
-    haha.add(line + '.txt')
+    haha.add(line[0 : len(line) - 1] + '.txt')
 
 if not os.path.isdir('big_temp'):
     os.mkdir('big_temp')
 
-for root, dire, filenames in os.walk('.'):
+for root, dire, filenames in os.walk('.', topdown=False):
     for filename in filenames:
         if filename in haha:
             copyfile(os.path.join(root, filename), os.path.join('big_temp', filename))
