fab340a10d5781c76b50ba2d56d7820cc0550aa8
HarrySurfaceBookWSL <Harry.Guo@outlook.com>
2019-12-24 16:27:51 +0800

    before fuck wash

---
+++ tmp_wash.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+std::vector<int> vertexList;
+std::vector<std::pair<int, int> > edgeList;
+
+char buff[256];
+
+int main(int argc, char *argv[]) {
+    std::ifstream ifs(argv[1]);
+    std::ofstream ofs(argv[2]);
+
+    int cnt = 0;
+
+    while (ifs.getline(buff, 256)) {
+        cnt = cnt + 1;
+        if (cnt == 1) continue;
+
+        int u, v;
+        std::stringstream ss;
+        ss << buff;
+        ss >> u >> v;
+
+        if (u == v) continue;
+
+        vertexList.push_back(u);
+        vertexList.push_back(v);
+
+        if (u > v) std::swap(u, v);
+
+        edgeList.push_back(std::make_pair(u, v));
+    }
+
+    std::sort(vertexList.begin(), vertexList.end());
+    auto it = std::unique(vertexList.begin(), vertexList.end());
+    vertexList.resize(std::distance(vertexList.begin(), it));
+
+    for (auto &e : edgeList) {
+        int &u = e.first;
+        int &v = e.second;
+
+        u = std::lower_bound(vertexList.begin(), vertexList.end(), u) - vertexList.begin() + 1;
+        v = std::lower_bound(vertexList.begin(), vertexList.end(), v) - vertexList.begin() + 1;
+    }
+
+    std::sort(edgeList.begin(), edgeList.end());
+    auto jt = std::unique(edgeList.begin(), edgeList.end());
+    edgeList.resize(std::distance(edgeList.begin(), jt));
+
+    int n = vertexList.size();
+    int m = edgeList.size();
+
+    std::ofstream ofs(argv[2]);
+
+    ofs << n << " " << m << std::endl;
+    for (auto e : edgeList) ofs << e.first << " " << e.second << std::endl;
+    return 0;
+}
---
+++ tmp_wash_fuck.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+t = sys.argv
+i = t[1]
+t = t[2]
+
+L = []
+
+for parent, dirnames, filenames in os.walk('misc'):
+    for filename in filenames:
+        name = os.path.join(parent, filename)
+
+        if not name[-3: len(name)] == 'txt':
+            continue
+
+        L.append(name)
+
+L.sort()
+
+i = int(i)
+
+os.system('./tmp_wash ' + L[i] + ' ' + 'misc_washed/' + L[i])
